0519cb03976f7fe00176e256685b0aaa21d169ca
Jon Bringhurst <jon@bringhurst.org>
2012-08-30 13:22:43 -0600

    Minor whitespace fix.

--- 2/even_fib.erl
+++ 2/even_fib.erl
@@ -1,5 +1,4 @@
 -module(even_fib).
-
 -export([even_fib/1]).
 
 even_fib(MaxValue) -> even_fib(1, 1, MaxValue, 0).
@@ -9,7 +8,7 @@ even_fib(_, Curr, Max, Acc) when Curr >= Max ->
 even_fib(B, Curr, Max, Acc) ->
     case (Curr rem 2) of
         0 ->
-            even_fib(Curr, Curr+B, Max, Acc + Curr);
+            even_fib(Curr, Curr+B, Max, Acc+Curr);
         _ ->
             even_fib(Curr, Curr+B, Max, Acc)
     end.
